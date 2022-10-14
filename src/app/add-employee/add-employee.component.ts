06d7ded6ffcbf3feb753f9dbaec8f2ab7b61768e
Riya505 <riyapk505@gmail.com>
2022-10-14 09:13:09 +0530

    new

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -9,6 +9,36 @@ export class AddEmployeeComponent implements OnInit {
 
   constructor() { }
 
+  employeeCode=""
+  employeeName=""
+  designation=""
+  gender=""
+  companyName=""
+  salary=""
+  companyAddress=""
+  mobile=""
+  companyEmail=""
+  yearofExprience=""
+  bloodGroup=""
+  dateOfBirth=""
+
+  readValues=()=>{
+    let data={
+      "employeeCode":this.employeeCode,
+  "employeeName":this.employeeName,
+  "designation":this.designation,
+  "gender":this.gender,
+  "companyName":this.companyName,
+  "salary":this.salary,
+  "companyAddress":this.companyAddress,
+  "mobile":this.mobile,
+  "companyEmail":this.companyEmail,
+  "yearofExprience":this.yearofExprience,
+  "bloodGroup":this.bloodGroup,
+  "dateOfBirth":this.dateOfBirth
+    }
+    console.log(data)
+  }
   ngOnInit(): void {
   }
 
